2d34e8526444bd1754c6c8c28513f5d163601f75
shiyuriaki <annagelisanga@gmail.com>
2025-10-20 18:16:40 -0700

    vercel error

--- src/Data/members.tsx
+++ src/Data/members.tsx
@@ -40,8 +40,8 @@ export const members = [
   { name: 'Danniella M.',  group: 3, role: 'Member',           committeeType: 'website',  img: '/Team_Photos/Danniella_Martinez.JPG' },
   { name: 'Deanna S.',     group: 3, role: 'Member',           committeeType: 'website',  img: '/Team_Photos/Deanna_Solis.JPG' },
   { name: 'Krisha H.',     group: 3, role: 'Member',           committeeType: 'website',  img: '/Team_Photos/Krisha_H.JPG' },
-  { name: 'Shreenithi T.', group: 3, role: 'Member',           committeeType: 'website' },
-  { name: 'Bryan C.',      group: 3, role: 'Member',           committeeType: 'website' },
+  { name: 'Shreenithi T.', group: 3, role: 'Member',           committeeType: 'website', img:'' },
+  { name: 'Bryan C.',      group: 3, role: 'Member',           committeeType: 'website', img:'' },
 
   // ===== DESIGN (Group 4) =====
   { name: 'Anita D.',      group: 4, role: 'Design Co-Lead',   committeeType: 'designer', img: '/Team_Photos/Anita_D.JPG' },
@@ -57,7 +57,7 @@ export const members = [
 
   { name: 'Mindy Y.',      group: 5, role: 'Member',           committeeType: 'sponsorship', img: '/Team_Photos/Mindy_Yun.JPG' },
   { name: 'Zarya A.',      group: 5, role: 'Member',           committeeType: 'sponsorship', img: '/Team_Photos/ZaryaAmin.jpeg' },
-  { name: 'Antonio L.',    group: 5, role: 'Member',           committeeType: 'sponsorship' },
-  { name: 'Brittanys T.',  group: 5, role: 'Member',           committeeType: 'sponsorship' },
+  { name: 'Antonio L.',    group: 5, role: 'Member',           committeeType: 'sponsorship', img:'' },
+  { name: 'Brittanys T.',  group: 5, role: 'Member',           committeeType: 'sponsorship', img: ''},
   { name: 'Sara T.',       group: 5, role: 'Member',           committeeType: 'sponsorship', img: '/Team_Photos/Sara_Tfaili.JPG' },
 ];
